88668b8dc42adf472ffc6e2c4372d18d16c89f23
isaacg11 <isaacgreywebdeveloper@gmail.com>
2015-10-03 13:26:25 -0700

    video version

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,27 +1,16 @@
-var ar = [1,3,2];
+
+var ar = [1,3,2,9,12,4,5,2];
 
 function bubbleSort(ar) {
-  var i, j, lim;
-  lim = ar.length ; while (lim--) {
-    for (i = 0, j = 1 ; i < lim ; ++i, ++j)
-      if (ar[i] > ar[j]) { temp = ar[i];  ar[i] = ar[j];  ar[j] = temp; }
-    console.log(temp);
-  }
-  return ar;
+	var i, j, lim;
+	lim = ar.length; while(lim--) {
+		for(i = 0, j = 1; i < lim; ++i, ++j)
+			if(ar[i] > ar[j]) { temp = ar[i]; ar[i]=ar[j]; ar[j]= temp;}
+	}
+	return ar;
 }
 console.log(bubbleSort(ar));
 
-
-// lim 2,1,0
-//------------
-// i = 0,1,0
-// j = 1,2,1
-//------------
-//ar[i] = 1,3,1
-//ar[j] = 3,2,3
-//------------
-//temp = 3,3,3
-//------------
 //1st loop doesn't change anything [1,3,2];
 //2nd loop swaps 3 with 2 [1,2,3]
 //3rd loop has to run because the 'while' statement is true
